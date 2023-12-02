c0ec770654bb69f092bcc3e925daba5c2eca3a32
nekolocation <edward.j.gilmore@gmail.com>
2023-12-02 06:53:19 -0800

    Final finished

--- final/assets/js/script.js
+++ final/assets/js/script.js
@@ -2,6 +2,11 @@ var buttonVariable = document.getElementById("toggleButton");
 var sqr = document.getElementById("squirrel");
 var track = 0;
 
+var audio1 = new Audio('assets/audio/noise1.mp3');
+var audio2 = new Audio('assets/audio/noise2.mp3');
+var audio3 = new Audio('assets/audio/noise3.mp3');
+var audio4 = new Audio('assets/audio/noise4.mp3');
+
 function toggleTheme() {
     let bodyID = document.querySelector("html");
     bodyID.classList.toggle("light-mode-bg");
@@ -17,4 +22,22 @@ function toggleTheme() {
     }
 }
 
+function squirrelTag() {
+    let rand = Math.random();
+    // console.log(rand);
+    if (rand < 0.25) {
+        audio1.play();
+    }
+    else if (rand < 0.50) {
+        audio2.play();
+    }
+    else if (rand < 0.75) {
+        audio3.play();
+    }
+    else {
+        audio4.play();
+    }
+}
+
 buttonVariable.onclick = toggleTheme;
+sqr.onclick = squirrelTag;
